5085a47fc36c6fff56f5a2cbdd166156eb0630ca
Shivam Anand <97366446+StellarShivam@users.noreply.github.com>
2023-08-08 19:27:33 +0530

    added cart and checkout frontend

--- src/App.js
+++ src/App.js
@@ -3,16 +3,8 @@ import { Routes, Route, Outlet } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
 import Home from "./routes/home/home.component";
 import Authentication from "./routes/authentication/authentication.component";
-
-const Shop = () => {
-  return (
-    <div>
-      <Outlet />
-      <h1>Welcome to Shop!!</h1>
-    </div>
-  );
-};
-
+import Shop from "./routes/shop/shop.component";
+import Checkout from "./routes/checkout/checkout.component";
 const App = () => {
   return (
     <Routes>
@@ -20,6 +12,7 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         <Route path="auth" element={<Authentication />} />
+        <Route path="checkout" element={<Checkout />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,8 +4,12 @@ import "./navigation.styles.scss";
 import { Fragment, useContext } from "react";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
+import { CartContext } from "../../contexts/cart.context";
 const Navigation = () => {
+  const { isCartOpen } = useContext(CartContext);
   const { currentUser, setCurrentUser } = useContext(UserContext);
   const signOutHandler = async () => {
     await signOutUser();
@@ -31,7 +35,9 @@ const Navigation = () => {
               SIGN IN
             </Link>
           )}
+          <CartIcon />
         </div>
+        {isCartOpen && <CartDropdown />}
       </div>
       <Outlet />
     </Fragment>
